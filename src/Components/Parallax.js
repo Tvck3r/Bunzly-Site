6932dd69997d51a3548a5cb458c869b63d238c66
Ya Bunz <yabunz01@gmail.com>
2019-03-09 18:38:03 -0500

    idk

--- src/Components/Parallax.js
+++ src/Components/Parallax.js
@@ -1,29 +1,11 @@
 import React from 'react';
-import '../App.css'
+import '../parallax.css'
 
 const Parallax = (props) => {
-
-    const style={
-       
-        /* The image used */
-        backgroundImage: 'url(' + props.imgUrl + ')',
-        
-        /* Set a specific height */
-        minHeight:props.minHeight, 
-        /* Create the parallax scrolling effect */
-        backgroundAttachment: 'fixed',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover'
-          
-        
-    }
-
     return (
-        <div style={style}>      
-        
+        <div class="wrapper">
+      
         </div>
-
     );
 }
 
