7cff048d0d435522dba1608ba8030696c5d2e703
Oliver <olivergillette@gmail.com>
2019-03-12 16:40:57 -0400

    Remove 404 css, update 404 page, update oliver resume page

--- src/pages/404Page.js
+++ src/pages/404Page.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './404Page.css'
 import PropTypes from 'prop-types';
 
 // Material-UI Components
@@ -8,6 +7,7 @@ import { withStyles } from '@material-ui/core/styles';
 const styles = theme => ({
   layout: {
     width: 'auto',
+    backgroundColor: '#dee4e7',
     paddingTop: `${theme.spacing.unit * 12}px`,
     marginLeft: theme.spacing.unit * 3,
     marginRight: theme.spacing.unit * 3,
@@ -18,24 +18,48 @@ const styles = theme => ({
     },
   },
   errorlink: {
-    font: 14,
-    textdecoration: 'none',
+    fontSize: '14px',
+    textDecoration: 'none',
     background: '#0046d5',
     display: 'inline-block',
-    padding: 30,
-    borderradius: 40,
+    padding: '15px 30px',
+    borderRadius: '40px',
     color: '#fff',
-    fontweight: 700,
+    fontWeight: 700,
+    WebkitBoxShadow: '0px 4px 15px -5px #0046d5',
+    boxShadow: '0px 4px 15px -5px #0046d5',
   },
-  bigerror: {
-    font: 230,
-    margin: 0,
-    fontweight: 900,
+  error404Id: {
     position: 'absolute',
+    left: '50%',
+    top: '50%',
+    WebkitTransform: 'translate(-50%, -50%)',
+    MsTransform: 'translate(-50%, -50%)',
+    transform: 'translate(-50%, -50%)',
+  },
+  error404: {
+    maxWidth: '410px',
+    width: '100%',
+  },
+  bigError: {
+    height: '280px',
+    position: 'relative',
+    zIndex: '-1',
+  },
+  bigErrorH1: {
+    fontSize: '230px',
+    margin: '0px',
+    fontwWight: 900,
+    position: 'absolute',
+    left: '50%',
+    webkitTransform: 'translate(-50%)',
+    msTransform: 'translate(-50%)',
+    transform: 'translate(-50%)',
     background: 'url("https://assets.saatchiart.com/saatchi/714211/art/2258352/1333561-7.jpg") no-repeat',
-    webkittextfillcolor: 'transparent',
-    backgroundsize: 'cover',
-    backgroundposition: 'center',
+    WebkitBackgroundClip: 'text',
+    WebkitTextFillColor: 'transparent',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
   },
 });
 
@@ -44,10 +68,10 @@ function Page404 (props) {
     const { classes } = props;
 
     return ( 
-      <div id="error404">
-        <div class="error404">
-          <div class="bigerror">
-            <h1 className={classes.bigerror}>Oops!</h1>
+      <div className={classes.error404Id}>
+        <div className={classes.error404}>
+          <div className={classes.bigError}>
+            <h1 className={classes.bigErrorH1}>Oops!</h1>
           </div>
           <br></br>
           <h2>404 - Page not found</h2>
